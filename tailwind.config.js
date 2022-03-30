ec61569d262b7d82e42424c80684753e26961906
lf-devs <devs@learnforce.app>
2022-03-30 22:28:43 +0500

    Dream Studio

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,6 @@ module.exports = {
         'iconShadow': '0px 10px 10px -5px rgba(0, 0, 0, 0.06), 0px 10px 15px -3px rgba(0, 0, 0, 0.1)',
         'Cardshadow': '0px 4px 6px -2px rgba(0, 0, 0, 0.06), 0px 20px 25px -5px rgba(0, 0, 0, 0.1)',
         'footerIcon': '0px 2px 5px rgba(0, 0, 0, 0.06), 0px 4px 6px -1px rgba(0, 0, 0, 0.1)',
-
-
       }
     },
   },
